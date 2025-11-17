997d929d915788d64b79ff87158dfc68fdb8f689
Alexis <work@alexisvarjo.fi>
2025-11-17 12:21:20 +0200

    laskurin asetus

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from counter import Counter
 
@@ -20,4 +20,11 @@ def increment():
 @app.route("/reset", methods=["POST"])
 def reset():
     cnt.reset()
+    return redirect("/")
+
+
+@app.route("/set", methods=["POST"])
+def set():
+    value = request.form.get("value")
+    cnt.set(int(value))
     return redirect("/")
